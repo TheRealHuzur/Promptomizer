9196a061f90f141d3d3bc2b5735c3c259a8b0989
TheRealHuzur <patrick.rosskothen@gmail.com>
2025-11-30 22:43:12 +0100

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = 'promptomizer-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    // Wir cachen hier auch externe Ressourcen, damit sie offline funktionieren (best effort)
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/lucide@latest'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS);
+            })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                // Not in cache - fetch from network
+                return fetch(event.request).then(
+                    (response) => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+// Activate Event (Cleanup old caches)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
