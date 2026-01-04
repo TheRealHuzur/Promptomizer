15d72b05b7b310938ac000b628933d27d14407cd
Patrick Rosskothen <patrick.rosskothen@gmail.com>
2026-01-04 17:16:41 +0100

    Namenskonflikt in db.js gelöst

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 // db.js - Verbindung zur Datenbank 🧠
 
 // 1. Konfiguration
-// WICHTIG: Die Werte müssen in 'Hochkommas' stehen!
 const SUPABASE_URL = 'https://nrrsroaubbpmjyexhuhi.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ycnNyb2F1YmJwbWp5ZXhodWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1MzU2ODcsImV4cCI6MjA4MzExMTY4N30.UcUIVDHiV6o5thTyeO8r5cylhPpNGl6Tpc3J0qsSxoM';
 
 // 2. Client starten
-// (Das "supabase"-Objekt kommt aus der Bibliothek, die wir im HTML geladen haben)
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// ACHTUNG: Wir nennen es jetzt "supabaseClient", damit es keinen Namensstreit gibt!
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // 3. Test-Signal senden
-console.log("Supabase Client wurde geladen! 🚀", supabase);
+console.log("Supabase Client wurde geladen! 🚀", supabaseClient);
